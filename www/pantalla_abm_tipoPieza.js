03487a5970d0083066481b6b1611c553885b6ed5
josetabuyo <josetabuyo@gmail.com>
2016-04-25 16:53:36 -0300

    JT: se agrega un refresh de pantalla piezas

--- www/pantalla_abm_tipoPieza.js
+++ www/pantalla_abm_tipoPieza.js
@@ -1,8 +1,8 @@
 var pantalla_abm_tipoPieza = {
 	start: function(){
-		
+		var pantalla = this;
 		var ui = $('#pantalla_abm_tipoPieza');
-		this.ui = ui;
+		pantalla.ui = ui;
 		
 		ui.on('show', function(){
 			
@@ -31,42 +31,42 @@ var pantalla_abm_tipoPieza = {
 			};
 			
 			datos.tipoPiezas[tipoPieza.id] = tipoPieza;
+			pantalla.appendTipoPieza(tipoPieza);
 			
-			var $tipoPieza_item = $('#plantilla_tipoPieza_item')
-								.clone()
-								.attr('id', 'item_' + tipoPieza.id)
-								.text( tipoPieza.descripcion );
-			
-			
-			
-			$tipoPieza_item.on('click', function(){
-				
-				
-				var tipoPieza = datos.tipoPiezas[$(this).attr('id').replace('item_','')];
-				
-				pantalla_abm_cota.setTipoPieza(tipoPieza)
-				
-				
-				
-			});
-			
-			ui.find('.list>ul').append($tipoPieza_item);
-			ui.find('#descripcion').val('');
 		});
+		
+		
+		pantalla.refresh();
 	},
-	
-	setTipoPieza: function(tipoPieza){
+	appendTipoPieza: function(tipoPieza){
 		var pantalla = this;
 		
+		var $tipoPieza_item = $('#plantilla_tipoPieza_item')
+							.clone()
+							.attr('id', 'item_' + tipoPieza.id)
+							.text( tipoPieza.descripcion );
 		
+		
+		
+		$tipoPieza_item.on('click', function(){
+			
+			var tipoPieza = datos.tipoPiezas[$(this).attr('id').replace('item_','')];
+			
+			pantalla_abm_cota.setTipoPieza(tipoPieza)
+			
+		});
+		
+		pantalla.ui.find('.list>ul').append($tipoPieza_item);
+		pantalla.ui.find('#descripcion').val('');
+		
+	},
+	refresh: function(){
+		var pantalla = this;
 		pantalla.ui.find('.list>ul').empty();
-		pantalla.ui.find('#tipoPieza_descripcion').text('Pieza: ' + tipoPieza.descripcion);
 		
-		pantalla.tipoPieza = tipoPieza;
-		
-		for(key in pantalla.tipoPieza.cotas){
-			var cota = pantalla.tipoPieza.cotas[key];
-			pantalla.appendCota(cota);
+		for(key in datos.tipoPiezas){
+			var tipoPieza = datos.tipoPiezas[key];
+			pantalla.appendTipoPieza(tipoPieza);
 		}
 	}
 };
